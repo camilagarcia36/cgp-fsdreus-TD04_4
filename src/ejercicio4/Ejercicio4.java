c3eaed3641afc82d7e1624ed9e67f502e597c415
camilagarcia36 <garciiaperezz@gmail.com>
2023-03-07 12:59:10 +0100

    ejercicio 4 finalizado

---
+++ src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,24 @@
+package ejercicio4;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int N = 40;
+		System.out.println("Valor de N: " + N);
+
+		
+		N += 77; 
+		
+		System.out.println("Valor de N despues del incremento: " + N);
+		
+		N -= 3; 
+		System.out.println("Valor de N despues de decrementar: "+ N);
+		
+		N*= 2; 
+		
+		System.out.println("Valor de N despues de duplicar: " + N);
+	}
+
+}
